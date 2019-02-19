98b9f4ca876c7944799a617f7f5a8f3263554c2e
Vodka <buxingyu@wdai.com>
2019-02-19 11:33:48 +0800

    基分类器修改

--- classification/__init__.py
+++ classification/__init__.py
@@ -3,7 +3,8 @@
 #author: buxy
 '''
 根据 sklearn 框架 创建基分类器，并引入一些自己的功能
-直接创建基分类器必须要有的函数: fit， predict，同时做一些通用的数据处理方面的操作
+直接创建基分类器必须要有的函数: fit， predict，用来处理一些通用的数据处理功能
+在下层中主要用来 _fit 和 _predict 来编写函数，不需要考虑数据结构和类型不对的问题
 '''
 
 import numpy as np
@@ -19,7 +20,9 @@ from .knn import KNN
 
 
 
-__all__ = []
+__all__ = [
+    'KNN'
+]
 
 
 class ClassifierEstimator(BaseEstimator,ClassifierMixin):
@@ -47,10 +50,10 @@ class ClassifierEstimator(BaseEstimator,ClassifierMixin):
         '''
         所有基分类器的通用环节(主要用于输入数据格式检查)
         曾考虑要不要用装饰器的方法实现，感觉应该没有这种方式方便
-        :param X: array-like， n_samples * n_features, 如果是单变量，期望是 n * 1 的结构
-        :param y: array-like， n_samples
+        :param X: array-like， n_samples * n_features， 如果是单变量，期望是 n * 1 的结构
+        :param y: array-like， n_samples，这里的y暂时限定只能是符合模型要求的数字，不可以是label
 
-        注意这里不 return self，return 的动作在下一步做
+        注意这里不 return self，return 的动作在下一层做
         '''
         self.X = self.to_array(X)
         self.y = self.to_array(y)
@@ -62,7 +65,7 @@ class ClassifierEstimator(BaseEstimator,ClassifierMixin):
             self.n_features = 1
             self.X = self.X.reshape(self.n_samples, self.n_features)
             logger.warning('X 可能是单变量，已经自动转置')
-
+        self._fit()
 
 
     def predict(self, test_data = None):
@@ -73,7 +76,24 @@ class ClassifierEstimator(BaseEstimator,ClassifierMixin):
         if test_data is None:
             self.test_data = self.X
         else:
+            # 需要判断一下数据格式是否准确
             self.test_data = self.to_array(test_data)
+            try:
+                self.n_test_samples, self.n_test_features = self.test_data.shape
+            except ValueError:
+                ## 说明 test_data 是单变量
+                self.n_test_samples = len(self.test_data)
+                self.n_test_features = 1
+                self.test_data = self.test_data.reshape(self.n_test_samples, self.n_test_features)
+                logger.warning('X 可能是单变量，已经自动转置')
+                if self.n_features != self.n_test_features:
+                    raise ValueError('测试数据特征数量与训练数据集不一致')
+        self._predict()
 
 
+    def _fit(self):
+        logger.info('该方法还未配置_fit方法')
 
+
+    def _predict(self):
+        logger.info('该方法还未配置_predict方法')
--- classification/knn.py
+++ classification/knn.py
@@ -8,11 +8,33 @@ knn 学习器
 '''
 
 import logging
+import numpy as np
 from . import ClassifierEstimator
 
 logger = logging.getLogger(__name__)
 
 class KNN(ClassifierEstimator):
-    def __init__(self):
+    def __init__(self, n, dist_func = 'euclid'):
+
+        self.n = n
+        if dist_func == 'euclid':
+            self.dist_func = self.euclid_dist
+        else:
+            assert hasattr(dist_func, '__call__')
+            self.dist_func = dist_func
         pass
-    
+
+
+    @staticmethod
+    def euclid_dist(x1, x2):
+        '''
+        欧几里得距离
+        :param x1: 一维向量, 1 * n 的格式
+        :param x2: 一维向量, 1 * n 的格式，内部进行转置
+        :return: float dist 距离
+        '''
+        x1_mat = np.mat(x1)
+        x2_mat = np.mat(x2)
+        delta = x1_mat - x2_mat
+        dist = np.sqrt(np.dot(delta, delta.T))
+        return dist[0,0]
