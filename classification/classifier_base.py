d201e466e9519b4f4ff9f1cbf2730888fac99e0b
Vodka <buxingyu@wdai.com>
2019-07-22 21:10:02 +0800

    优化基类

--- classification/classifier_base.py
+++ classification/classifier_base.py
@@ -2,47 +2,50 @@
 # created_date: 2019-05-15
 # author: buxy
 """
-根据 sklearn 框架 创建基分类器，并引入一些自己的功能
-直接创建基分类器必须要有的函数: fit， predict，用来处理一些通用的数据处理功能
-在子类中主要用来 _fit 和 _predict 来编写函数，不需要考虑数据结构和类型不对的问题
+根据 sklearn 框架 创建基分类器
+直接创建基分类器必须要有的: fit、predict
+在子类中主要编写_fit,_predict, 有关X y的各种基本性质初始化已经做了，不需要再关心了
 """
 
 import numpy as np
-import logging
+import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
 
 
 class ClassifierEstimator(BaseEstimator, ClassifierMixin):
 
     def __init__(self):
-        """加入一个日志功能"""
         super().__init__()
-        self.logger = logging.getLogger(__name__)
         self.X = None
         self.y = None
         self.n_samples = None
         self.n_features = None
+        self.feature_names = None
         self.test_data = None
         self.n_test_samples = None
         self.n_test_features = None
-        self.test_result = None
 
     @staticmethod
     def to_array(data):
         """
         将输入数据转换成 np.array
-        如果转换不成功则直接报错
+        如果转换不成功则报错
         :param data: 数据，期望是 array-like, 但有可能不是
         """
         if not isinstance(data, np.ndarray):
             try:
-                data = np.array(data)
+                data = np.asarray(data)
             except Exception as e:
                 raise ValueError('期望数据是 np.ndarray 格式: {0}'.format(e.__str__()))
         return data
 
-    def format_data(self, data):
-        """返回数据的n_sample，n_feature，并且当是一维向量时，进行转置"""
+    @staticmethod
+    def format_data(data):
+        """
+        格式化数据，转换成n_samples * n_features 的格式，如果不是则强行转
+        :param data: 特征
+        :return: 特征, n_samples, n_features
+        """
         shape = data.shape
         if len(shape) > 1:
             n_samples, n_features = shape
@@ -50,42 +53,39 @@ class ClassifierEstimator(BaseEstimator, ClassifierMixin):
             n_samples = len(data)
             n_features = 1
             data = data.reshape(n_samples, n_features)
-            self.logger.warning('X 可能是单变量，已经自动转置')
         return data, n_samples, n_features
 
-    def fit(self, X, y):
+    def fit(self, X, y, feature_names=None):
         """
-        所有基分类器的通用环节(主要用于输入数据格式检查)
-        曾考虑要不要用装饰器的方法实现，感觉应该没有这种方式方便
-        :param X: array-like， n_samples * n_features， 如果是单变量，期望是 n * 1 的结构
-        :param y: array-like， n_samples，这里的y暂时限定只能是符合模型要求的数字，不可以是label
+        :param X: array-like, n_samples * n_features， 如果是单变量，期望是 n * 1 的结构
+        :param y: array-like, n_samples，这里的y暂时限定只能是符合模型要求的数字，不可以是label
+        :param feature_names: list n_features 可以为None
         """
         X = self.to_array(X)
         self.y = self.to_array(y)
         self.X, self.n_samples, self.n_features = self.format_data(X)
+        if feature_names is None:
+            if isinstance(X, pd.DataFrame):
+                self.feature_names = X.columns.tolist()
+            else:
+                self.feature_names = list(range(self.n_features))
+        else:
+            assert len(feature_names) == self.n_features, 'feature_names和n_features长度不一致'
+            self.feature_names = feature_names
         self._fit()
         return self
 
-    def predict(self, X=None):
+    def predict(self, X):
         """
-        当 X 是 None 的时候，将 训练数据 X 赋予 test_data
         :param X: array-like, 会检查一下格式是否准确
         """
-        if X is None:
-            self.test_data = self.X
-            self.n_test_samples = self.n_samples
-            self.n_test_features = self.n_features
-        else:
-            test_data = self.to_array(X)
-            self.test_data, self.n_test_samples, self.n_test_features = self.format_data(test_data)
-            assert self.n_features == self.n_test_features, '测试数据与训练数据变量长度不一致'
-        self.test_result = self._predict()
-        return self.test_result
+        test_data = self.to_array(X)
+        self.test_data, self.n_test_samples, self.n_test_features = self.format_data(test_data)
+        assert self.n_features == self.n_test_features, '测试数据与训练数据变量长度不一致'
+        return self._predict()
 
     def _fit(self):
-        self.logger.info('该对象还未配置_fit方法')
+        raise NotImplementedError('还未完成_fit')
 
     def _predict(self):
-        self.logger.info('该对象还未配置_predict方法')
-        self.test_result = None
-        return None
+        raise NotImplementedError('还未完成_predict')
--- preprocess/preprocess_base.py
+++ preprocess/preprocess_base.py
@@ -7,23 +7,21 @@
 
 
 import numpy as np
-import logging
+import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class PreprocessTransformer(BaseEstimator, TransformerMixin):
-    """标准数据预处理用Transformer"""
+
     def __init__(self):
         super().__init__()
-        self.logger = logging.getLogger(__name__)
-
         self.X = None
         self.y = None
+        self.feature_names = None
         self.n_samples = None
         self.n_features = None
         self.transform_data = None
         self.transform_y = None
-        self.transformed_data = None
         self.n_transform_samples = None
         self.n_transform_features = None
 
@@ -31,18 +29,23 @@ class PreprocessTransformer(BaseEstimator, TransformerMixin):
     def to_array(data):
         """
         将输入数据转换成 np.array
-        如果转换不成功则直接报错
+        如果转换不成功则报错
         :param data: 数据，期望是 array-like, 但有可能不是
         """
         if not isinstance(data, np.ndarray):
             try:
-                data = np.array(data)
+                data = np.asarray(data)
             except Exception as e:
                 raise ValueError('期望数据是 np.ndarray 格式: {0}'.format(e.__str__()))
         return data
 
-    def format_data(self, data):
-        """返回数据的n_sample，n_feature，并且当是一维向量时，进行转置"""
+    @staticmethod
+    def format_data(data):
+        """
+        格式化数据，转换成n_samples * n_features 的格式，如果不是则强行转
+        :param data: 特征
+        :return: 特征, n_samples, n_features
+        """
         shape = data.shape
         if len(shape) > 1:
             n_samples, n_features = shape
@@ -50,14 +53,13 @@ class PreprocessTransformer(BaseEstimator, TransformerMixin):
             n_samples = len(data)
             n_features = 1
             data = data.reshape(n_samples, n_features)
-            self.logger.warning('X 可能是单变量，已经自动转置')
         return data, n_samples, n_features
 
-    def fit(self, X, y=None):
+    def fit(self, X, y=None, feature_names=None):
         """
-        所有transformer的通用环节(主要用于输入数据格式检查)
-        :param X: array-like， n_samples * n_features， 如果是单变量，期望是 n * 1 的结构
-        :param y: array-like， n_samples，可选 ,这里的y暂时限定只能是符合模型要求的数字，不可以是label
+        :param X: array-like, n_samples * n_features， 如果是单变量，期望是 n * 1 的结构
+        :param y: array-like, n_samples，这里的y暂时限定只能是符合模型要求的数字，不可以是label
+        :param feature_names: list n_features 可以为None
         """
         data = self.to_array(X)
         self.X, self.n_samples, self.n_features = self.format_data(data)
@@ -65,32 +67,30 @@ class PreprocessTransformer(BaseEstimator, TransformerMixin):
             self.y = self.to_array(y)
         else:
             self.y = None
+        if feature_names is None:
+            if isinstance(X, pd.DataFrame):
+                self.feature_names = X.columns.tolist()
+            else:
+                self.feature_names = list(range(self.n_features))
+        else:
+            assert len(feature_names) == self.n_features, 'feature_names和n_features长度不一致'
+            self.feature_names = feature_names
         self._fit()
         return self
 
-    def transform(self, X=None, y=None):
-        """
-        当 test_data 是 None 的时候，将 X 赋予 test_data
-        """
-        if X is None:
-            self.transform_data = self.X
-            self.n_transform_samples = self.n_samples
-            self.n_transform_features = self.n_features
-            self.transform_y = self.y
-        else:
-            # 需要判断一下数据格式是否准确
-            transform_data = self.to_array(X)
-            self.transform_data, self.n_transform_samples, self.n_transform_features = self.format_data(transform_data)
-            assert self.n_features == self.n_transform_features, '测试数据与训练数据变量长度不一致'
+    def transform(self, X, y=None):
+        # 需要判断一下数据格式是否准确
+        transform_data = self.to_array(X)
+        self.transform_data, self.n_transform_samples, self.n_transform_features = self.format_data(transform_data)
+        assert self.n_features == self.n_transform_features, '测试数据与训练数据变量长度不一致'
         if y is not None:
             self.transform_y = self.to_array(y)
-        self.transformed_data = self._transform()
-        return self.transformed_data
+        else:
+            self.transform_y = None
+        return self._transform()
 
     def _fit(self):
-        self.logger.info('该对象还未配置_fit方法')
+        raise NotImplementedError('该对象还未配置_fit方法')
 
     def _transform(self):
-        self.logger.info('该对象还未配置_transform方法')
-        transformed_data = None
-        return transformed_data
+        raise NotImplementedError('该对象还未配置_transform方法')
